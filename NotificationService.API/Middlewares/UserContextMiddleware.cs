4fbbe18abe8bcac45c065a69456bd4abeca522fd
Conra <conra.arq@gmail.com>
2025-02-14 02:48:11 +0100

    Refactoring

--- NotificationService.API/Middlewares/UserContextMiddleware.cs
+++ NotificationService.API/Middlewares/UserContextMiddleware.cs
@@ -6,21 +6,19 @@ namespace NotificationService.API.Middlewares
     public class UserContextMiddleware
     {
         private readonly RequestDelegate _next;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
         /// <summary>
         /// Middleware responsible for setting the current user's ID in the DbContext.
         /// This ID is automatically assigned to the CreatedBy and LastModifiedBy properties of each entity during data operations.
         /// </summary>
-        public UserContextMiddleware(RequestDelegate next, IHttpContextAccessor httpContextAccessor)
+        public UserContextMiddleware(RequestDelegate next)
         {
             _next = next;
-            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task InvokeAsync(HttpContext context, NotificationServiceDbContext dbContext)
         {
-            var userId = _httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "System";
+            var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value ?? "System";
             dbContext.CurrentUserId = userId;
 
             await _next(context);
